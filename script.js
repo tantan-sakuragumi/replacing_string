9bda7db4904ab4ba43ee238d2d9252988d8d21d7
tantan-sakuragumi <58903058+tantan-sakuragumi@users.noreply.github.com>
2021-08-23 09:01:44 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const replaceAllStrings = () => {
+    let replacedString = document.getElementById("userInput").value;
+    for (const box of document.getElementsByClassName("ruleBox")) {
+        const inputElems = box.getElementsByTagName("input");
+        replacedString = replacedString.split(inputElems[0].value).join(inputElems[1].value);
+    }
+    document.getElementById("resultOutput").value = replacedString;
+}
+document.body.addEventListener("input", replaceAllStrings);
+
+document.body.addEventListener("click", e => {
+    if(e.target.id === "btnToAddRuleBox" || e.target.classList.contains("crossSymbol")) {
+        const newElm = document.createElement("div");
+        newElm.classList.add("ruleBox");
+        newElm.innerHTML = `
+            <input placeholder="置き換えられる文字列">
+            <input placeholder="置き換える文字列">
+            <button class="btnToDeleteRuleBox">削除</button>
+        `;
+        document.getElementById("ruleBoxContainer").insertBefore(newElm, document.getElementById("btnToAddRuleBox"));
+    } else if(e.target.classList.contains("btnToDeleteRuleBox")) { 
+        e.target.parentNode.remove();
+        replaceAllStrings()
+    
+    }
+});
